6c0b2eeeff9ac13438dd9531c60f71194068c958
Nabil <lahlalia.nabil@hotmail.com>
2023-04-05 18:05:05 +0000

    CRUD pagination recherche

--- proje/hopital/urls.py
+++ proje/hopital/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import list_patients,create_patient,delete_patient
+from .views import list_patients,create_patient,delete_patient,update_patient,search
 
 urlpatterns = [
     path('patients/', list_patients, name='list_patients'),
     path('create/', create_patient, name='create_patient'),
+    path('search/', search, name='search'),
     path('delete/<int:pk>', delete_patient, name='delete_patient'),
+    path('update/<int:pk>', update_patient, name='update_patient'),
 
 ]
--- proje/hopital/views.py
+++ proje/hopital/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from .models import Patient
 from faker import Faker
 from .form import PatientForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 
@@ -14,7 +15,15 @@ def list_patients(request):
         Patient(None,f.name(),f.date(),).save()
         '''
 
-    patients = Patient.objects.all()
+    all_patients = Patient.objects.all()
+    p = Paginator(all_patients, 5)
+    page = request.GET.get('page',1)
+    try:
+        patients = p.page(page)
+    except PageNotAnInteger:
+        patients = p.page(1)
+    except EmptyPage:
+        patients = p.page(p.num_pages)
     return render(request,'patients.html',{'patients': patients})
 
 def create_patient(request):
@@ -32,3 +41,18 @@ def delete_patient(request, pk):
     patient.delete()
     return redirect('list_patients')
 
+def update_patient(request,pk):
+    patient = get_object_or_404(Patient, pk=pk)
+    if request.method == 'POST':
+        form = PatientForm(request.POST,instance=patient)
+        if form.is_valid():
+            form.save()
+            return redirect('list_patients')
+    else:
+        form = PatientForm(instance=patient)
+    return render(request, 'update_patient.html',{'form': form, 'patient':patient})
+
+def search(request):
+    query = request.GET.get('keyword')
+    patients = Patient.objects.filter(nom__contains=query)
+    return render(request,'patients.html',{'patients': patients})
